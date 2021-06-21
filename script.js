a0fc3cc30fd163777b97f4cd823e684b711ba139
rishin <mrishinv@gmail.com>
2021-06-21 22:36:42 +0530

    progressbar mathfunction updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
-let progressBar = document.querySelector(".progress-bar");
+// let progressBar = document.querySelector(".progress-bar");
+
 
 //Setting Default video is not playing
 let isVideoPlaying = false;
@@ -36,7 +37,11 @@ function playOrPauseVideo() {
 
 function updateProgressBar(event) {
     console.log(event.target.currentTime, event.target.duration);
-    progressBar.style.cssText = ` width: ${event.target.currentTime/event.target.duration*100}% `;
+    // one method
+    //progressBar.style.cssText = ` width: ${event.target.currentTime/event.target.duration*100}% `;
+    // exact method is given below
+    let width = Math.floor(100 * event.target.currentTime / event.target.duration);
+    document.querySelector(".progress-bar").style.width = width+'%';
 }
 
 // AddEventListeners
