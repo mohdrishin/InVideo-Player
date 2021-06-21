6577fdbb9d47aa7c3b386046b0ffc396986c3f0d
rishin <mrishinv@gmail.com>
2021-06-21 23:03:36 +0530

    seekbar updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
 let progressBar = document.querySelector(".progress-bar");
-
+let progressRange = document.querySelector(".progress-range");
 
 //Setting Default video is not playing
 let isVideoPlaying = false;
@@ -41,15 +41,26 @@ function updateProgressBar(event) {
     let currentTime = event.target.currentTime;
     let duration = event.target.duration;
     progressBar.style.cssText = ` width: ${currentTime/duration*100}% `;
-    console.log(progressBar);
+    // console.log(progressBar);
     // second method is given below
     // let width = Math.floor(100 * event.target.currentTime / event.target.duration);
     // document.querySelector(".progress-bar").style.width = width+'%';
+}
+
+function updateSeekbar(event) {
+    // console.log(event.offsetX, event.target.clientWidth);
+    let currentPoint = event.offsetX;
+    let progressbarWidth = event.target.clientWidth;
+    let currentRange = (currentPoint/progressbarWidth) * video.duration
+    // console.log(currentRange)
+    // below currentTime is a inbuilt property of video
+    video.currentTime = currentRange;
 }
 
 // AddEventListeners
 playBtn.addEventListener("click", playOrPauseVideo);
 video.addEventListener("click", playOrPauseVideo);
 video.addEventListener("timeupdate", updateProgressBar); 
+progressRange.addEventListener("click", updateSeekbar);
 
 // video.addEventListener("keyDown", playOrPauseKeyBoard);
